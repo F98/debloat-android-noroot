9401339fdeb646aa33ccedfe2c3dc3bd932f3107
NinetyEight <70932827+Ninety-Eight@users.noreply.github.com>
2020-10-01 23:41:23 -0400

    more simplification because making smaller better

--- run.py
+++ run.py
@@ -7,19 +7,18 @@ d = "files/"
 dto = str(datetime.date.today())
 rng = random.randrange(1,255)
 otf = "logs/removed-packages-" + str(rng) + "-" + dto + ".txt"
+rt = 0
 if ic == "1":
-    fi = d + "ev.txt"
-    rt = 1
+    fi = "ev.txt"
 if ic == "2":
-    fi = d + "abg.txt"
-    rt = 1
+    fi = "abg.txt"
 if ic == "3":
-    fi = d + "ue.txt"
-    rt = 0
+    fi = "ue.txt"
+    rt = 1
 if ic not in o:
     print("Cancelled!")
     exit()
-with open(fi) as f:
+with open(d + fi) as f:
     c = f.readlines()
 g = [x.strip("\n") for x in c]
 sr = []
@@ -28,15 +27,14 @@ while g:
         u = x.split()
         t = u[-1]
         try:
-            if rt == 1: et = "pm uninstall -k --user 0 "
-            if rt == 0: et = "cmd package install-existing "
-            cmdx = et + x
-            y = subprocess.check_output("adb shell " + cmdx, shell=True)
+            if rt == 1: et = "cmd package install-existing "
+            if rt != 1: et = "pm uninstall -k --user 0 "
+            y = subprocess.check_output("adb shell " + et + x, shell=True)
             z = ''.join(filter(str.isalnum, y.decode("utf-8")))
             print("Successfully Removed " + t)
             g.remove(x)
             sr.append(t)
-        except Exception:
+        except:
             print(t + " is not installed so not removed")
             g.remove(x)
 with open(otf, 'w') as f:
